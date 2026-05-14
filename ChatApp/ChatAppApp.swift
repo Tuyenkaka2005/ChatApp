2f78d19bec36e18e448012b9ba92c9c8aebbbe17
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-13 21:48:36 -0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Tuyen on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
